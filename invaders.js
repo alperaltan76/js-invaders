b1a4c0d19fdbee1922b0ee602ad6944f6c7e2c8d
alperaltan76 <94382944+alperaltan76@users.noreply.github.com>
2022-05-10 09:59:31 +0200

    game finished

---
+++ invaders.js
@@ -0,0 +1,69 @@
+let player = {
+    x: 400
+    
+};
+
+
+// function setup() {
+//     let canvas = document.getElementById('invaders-canvas');
+//     let context = canvas.getContext('2d')
+
+//     context.fillStyle = "black";
+//     context.fillRect(0,0,800,600);
+
+//     context.fillStyle = "white";
+//     context.font = '48px serif';
+//     context.fillText("Space Invaders",10,50);
+
+//     context.fillStyle = "yellow"
+//     // context.fillRect(390,580,20,20);
+//     context.beginPath();
+//     context.moveTo(player.x, 580);
+//     context.lineTo(400, 580);
+//     context.lineTo(390, 600);
+//     context.lineTo(410, 600);
+//     context.fill();
+
+// }
+
+function draw() {
+    let canvas = document.getElementById('invaders-canvas');
+    let context = canvas.getContext('2d')
+
+    context.fillStyle = "black";
+    context.fillRect(0,0,800,600);
+
+    context.fillStyle = "white";
+    context.font = '48px serif';
+    context.fillText("Space Invaders",10,50);
+
+    context.fillStyle = "yellow"
+    // context.fillRect(390,580,20,20);
+    context.beginPath();
+    context.moveTo(player.x, 580);
+    context.lineTo(player.x-10, 600);
+    context.lineTo(player.x+10, 600);
+    context.fill();
+
+}
+
+function setup() {
+    draw();
+}
+function movePlayer(event) {
+    switch(event.key) {
+        case "ArrowLeft":
+            player.x -=10;
+            //
+            break;
+            case "ArrowRight":
+                player.x +=10;
+                //
+                break;
+    }
+    draw();
+}
+
+
+window.addEventListener('load',setup);
+window.addEventListener('keydown',movePlayer);
